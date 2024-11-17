cd87e3b82b9939d7de4a4995ccd47a9edcafdfa0
root <jeny.munoz@educa.madrid.org>
2024-11-17 17:13:27 +0100

    modificando el archivo build.sh para corregir errores de creación de los archivos html

--- build.sh
+++ build.sh
@@ -1,14 +1,36 @@
-#!usr/bin/bash
+#!/usr/bin/bash
+
+echo "iniciando tarea 01"
+echo "descargando el archivo de internet..."
+wget -q https://www.w3schools.com/w3css/4/w3.css
+
+if [ $? = 0 ]; then
+	echo "descarga correcta"
+else
+	echo "descarga incorrecta"
+fi
+
+echo $?
+
+if [ -e w3.css ]; then
+	echo "correcto. Ahora el archivo puede ser utilizado"
+else
+	echo "Este archivo no existe"
+	echo "Descargando..."
+	wget  https://www.w3schools.com/w3css/4/w3.css
+	echo "tarea terminada"
+fi
+echo "tarea terminiada"
 
 #Vamos a crear nuestros archivos.html
-
+echo "iniciando tarea 02"
 echo "creando cabecera.html"
 touch cabecera.html
 
 echo "creando cuerpo.html"
 touch cuerpo.html
 
-echo "creando pie.html
+echo "creando pie.html"
 touch pie.html
 
 echo "tarea terminada"
@@ -21,10 +43,11 @@ cat << EOF >> cabecera.html
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="Stylesheet" href="w3.css">
     <title>Cabecera</title>
 </head>
 <body>
-    <header>
+    <header class='w3-blue'>
         <nav>
             <p><a href="index.html">index.html</a></p>
             <p><a href="pie.html">pie.html</a></p>
@@ -46,9 +69,10 @@ cat << EOF >> cuerpo.html
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="Stylesheet" href="w3.css">
+    <title>cuerpo</title>
 </head>
-<body>
+<body class='w3-red'>
     <h1>Elementos que pueden ir dentro de la etiqueta "<body></body>" </h1>
 </body>
 </html>
@@ -63,10 +87,12 @@ cat << EOF >> pie.html
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="Stylesheet" href="w3.css">
     <title>Document</title>
 </head>
 <body>
     <h1>Elementos que pueden ir dentro de una etiqueta "<footer></footer>" </h1>
+    <footer class='w3-green'></footer>
 </body>
 </html>
 
@@ -74,13 +100,4 @@ EOF
 echo "tarea realizada"
 
 ############################################
-echo "vamos a descargar el archivo que contiene los estilos"
-wget https://www.w3schools.com/w3css/4/w3.css
 
-echo "comprobando si la ejecución fue correcta"
-if[[ wget -ne 0 ]];then
-echo "descarga correcta"
-else
-echo "descarga incorecta"
-exit 1
-fi
